e1bdf652af2a1270172f815adf4ca85d4ba2c852
yzj <darkblue.young@foxmail.com>
2016-04-02 14:17:00 +0800

    换行写入文件

--- pachong.py
+++ pachong.py
@@ -69,7 +69,7 @@ class JDPC:
     #写入文件
     def writeData(self,content):
         for item in content:
-            self.file.write(item)
+            self.file.write(item + '\n')
 
 
 
@@ -77,24 +77,3 @@ spider = JDPC()
 spider.getAllUrl()
 spider.setFileTitle("urls")
 spider.writeData(spider.mobileUrls)
-
-
-
-"""
-values = {}
-values['cat'] = "9987,653,655"
-values['page'] = "2"
-values['go'] = "0"
-values['JL'] = "6_0_0"
-data = urllib.urlencode(values)
-url = "http://list.jd.com/list.html"
-geturl = url + '?' + data
-request = urllib2.Request(geturl)
-response = urllib2.urlopen(request)
-content = response.read()
-pattern = re.compile('<div.*?j-sku-item.*?<a.*?href="(.*?)" >',re.S)
-items = re.findall(pattern,content)
-print items[0]
-for item in items:
-    print item
-"""
