c1ac96f3872bf50ff2d045924d86ce649d47df35
yzj <darkblue.young@foxmail.com>
2016-04-04 12:30:22 +0800

    初步完成异常处理

--- pachong4.py
+++ pachong4.py
@@ -12,8 +12,13 @@ class JdPrice(object):
     """
     def __init__(self, url):
         self.url = url
-        self._response = urllib.urlopen(self.url)
-        self.html = self._response.read()
+        self.flag = True
+        try:
+            self._response = urllib.urlopen(self.url)
+            self.html = self._response.read()
+        except Exception,e:
+            print 'init error:{0}'.format(e.message)
+            self.flag = False
         self.attrsDict = {}
         self.file = None
 
@@ -21,8 +26,12 @@ class JdPrice(object):
         """
         获取html中，商品的描述(未对数据进行详细处理，粗略的返回str类型)
         """
-        product_re = re.compile(r'compatible: true,(.*?)};', re.S)
-        product_info = re.findall(product_re, self.html)[0]
+        try:
+            product_re = re.compile(r'compatible: true,(.*?)};', re.S)
+            product_info = re.findall(product_re, self.html)[0]
+        except IndexError,e:
+            print 'get_product error:{0}'.format(e.message)
+            return None
         return product_info
 
     def get_product_skuid(self):
@@ -30,8 +39,14 @@ class JdPrice(object):
         通过获取的商品信息，获取商品的skuid
         """
         product_info = self.get_product()
-        skuid_re = re.compile(r'skuid: (.*?),')
-        skuid = re.findall(skuid_re, product_info)[0]
+        if not product_info:
+            return None
+        try:
+            skuid_re = re.compile(r'skuid: (.*?),')
+            skuid = re.findall(skuid_re, product_info)[0]
+        except IndexError,e:
+            print 'get_product error：' + e.message
+            return None
         return skuid
 
     def get_product_name(self):
@@ -40,9 +55,15 @@ class JdPrice(object):
         """
         #'\u4e2d\u6587'.decode('unicode-escape') （你可能需要print它才能看到结果）
         product_info = self.get_product()
+        if not product_info:
+            return None
         #源码中名称左右有两个',所以过滤的时候应该去掉
-        name_re = re.compile(r"name: '(.*?)',")
-        name = re.findall(name_re, product_info)[0]
+        try:
+            name_re = re.compile(r"name: '(.*?)',")
+            name = re.findall(name_re, product_info)[0]
+        except IndexError,e:
+            print 'get_product_name error:' + e.message
+            return None
         return name.decode('unicode-escape')#将其转换为中文
 
     def get_product_price(self):
@@ -54,6 +75,8 @@ class JdPrice(object):
 
         #得到产品的序号和名称，取价格的时候会用得到
         skuid = self.get_product_skuid()
+        if not skuid:
+            return None
         #name = self.get_product_name()
         #print name
 
@@ -79,19 +102,28 @@ class JdPrice(object):
             attrs_re = re.compile(r'<tr><td.*?">(.*?)</td><td>(.*?)</td></tr>',re.S)
             attrs = re.findall(attrs_re,attrsText)
         except IndexError,e:
-            print "error:" + e.message
-            attrs = ['错误','未知']
+            print "get_product_attrs error:" + e.message
+            return None
         for attr in attrs:
             key = attr[0]
             value = attr[1]
             self.attrsDict.update({key:value})
+        return True
 
     def save_attrs(self):
         """
         将获得的参数信息保存到txt文件
         :return:
         """
-        self.get_product_attrs()
+        if not self.flag:
+            print "访问失败，跳过"
+            return None
+        if not self.get_product_attrs():
+            print "1"
+            return None
+        if not self.get_product_name():
+            print"2"
+            return None
         title = self.get_product_name()
         try:
             self.file = open('products\\'+title+'.txt','w+')
@@ -107,6 +139,10 @@ if __name__ == '__main__':
     start = time.clock()
     reload(sys)
     sys.setdefaultencoding('utf-8')
+    url = "http://item.jd.com/1861098.html"
+    jp = JdPrice(url)
+    jp.save_attrs()
+    """
     print "+"*20+"welcome to 京东放养的爬虫"+"+"*20
     i=0
     file = open("urls.txt")
@@ -121,92 +157,10 @@ if __name__ == '__main__':
     print i
     end = time.clock()
     print end-start
+    """
 
 
 print "+"*20+"welcome to 京东放养的爬虫"+"+"*20
 
-
-'''
-    def get_product_brand(self):
-        """
-        获取html中商品的品牌
-        :return:
-        """
-        brand_re = re.compile(r'product-detail-2.*?<td>(.*?)</td>',re.S)
-        brand = re.findall(brand_re,self.html)[0]
-        return brand.decode('gbk')
-
-    def get_product_modelnumber(self):
-        """
-        获取html中商品型号
-        :return:
-        """
-        modelnumber_re = re.compile(r'product-detail-2.*?<td>.*?<td>(.*?)</td>',re.S)
-        modelnumber = re.findall(modelnumber_re,self.html)[0]
-        return modelnumber
-
-    def get_product_system(self):
-        """
-        获取html中商品系统
-        :return:
-        """
-        system_re = re.compile(r'product-detail-2.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>(.*?)</td>',re.S)
-        system = re.findall(system_re,self.html)[0]
-        return system
-
-    def get_product_cpu(self):
-        """
-        获取html中商品cpu
-        :return:
-        """
-        cpu_re = re.compile(r'product-detail-2.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>(.*?)</td>',re.S)
-        cpu = re.findall(cpu_re,self.html)[0]
-        return cpu.decode('gbk')
-
-    def get_product_ROM(self):
-        """
-        获取html中的机身内存
-        :return:
-        """
-        ROM_re = re.compile(r'product-detail-2.*?colspan.*?colspan.*?colspan.*?<td>(.*?)</td>',re.S)
-        ROM = re.findall(ROM_re,self.html)[0]
-        return ROM
-
-    def get_product_RAM(self):
-        """
-        获取html中的运行内存
-        :return:
-        """
-        RAM_re = re.compile(r'product-detail-2.*?colspan.*?colspan.*?colspan.*?<td>.*?<td>(.*?)</td>',re.S)
-        RAM = re.findall(RAM_re,self.html)[0]
-        return RAM
-
-    def get_product_size(self):
-        """
-        获取html中屏幕尺寸
-        :return:
-        """
-        size_re = re.compile(r'product-detail-2.*?colspan.*?colspan.*?colspan.*?colspan.*?<td>(.*?)</td>',re.S)
-        size = re.findall(size_re,self.html)[0]
-        return size.decode('gbk')
-
-    def get_product_dpi(self):
-        """
-        获取html中屏幕分辨率
-        :return:
-        """
-        dpi_re = re.compile(r'product-detail-2.*?colspan.*?colspan.*?colspan.*?colspan.*?<td>.*?<td>.*?<td>(.*?)</td>',re.S)
-        dpi = re.findall(dpi_re,self.html)[0]
-        return dpi
-
-    def get_product_weight(self):
-        """
-        获取机身重量
-        :return:
-        """
-        weight_re = re.compile(r'product-detail-2.*?colspan.*?colspan.*?colspan.*?colspan.*?colspan.*?colspan.*?colspan.*?colspan.*?colspan.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>.*?<td>(.*?)</td>',re.S)
-        weight = re.findall(weight_re,self.html)[0]
-        return weight
-'''
 
 
